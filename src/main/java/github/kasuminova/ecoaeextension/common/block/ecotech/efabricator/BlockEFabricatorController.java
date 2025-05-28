3ea70bd5de26f5fe25d1ab27e67b0711197c54e1
6666614li <2157995671a@gmail.com>
2025-05-28 17:12:18 +0800

    Update BlockEFabricatorController.java

--- src/main/java/github/kasuminova/ecoaeextension/common/block/ecotech/efabricator/BlockEFabricatorController.java
+++ src/main/java/github/kasuminova/ecoaeextension/common/block/ecotech/efabricator/BlockEFabricatorController.java
@@ -83,15 +83,6 @@ public class BlockEFabricatorController extends BlockController {
                 }
             }
 
-            UUID ownerUUID = ctrl.getOwner();
-            Item dropped = getItemDropped(state, rand, damageDropped(state));
-            ItemStack stackCtrl = new ItemStack(dropped, 1);
-            if (ownerUUID != null) {
-                NBTTagCompound tag = new NBTTagCompound();
-                tag.setString("owner", ownerUUID.toString());
-                stackCtrl.setTagCompound(tag);
-            }
-            spawnAsEntity(worldIn, pos, stackCtrl);
         }
 
         worldIn.removeTileEntity(pos);
